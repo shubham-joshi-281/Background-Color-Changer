df26ad4256757f62457064718d7d83897a574ea9
Shubham <shubhamjoshi281@gmail.com>
2023-09-22 12:59:12 +0530

    js file added

---
+++ index.js
@@ -0,0 +1,22 @@
+const btn = document.querySelectorAll("button");
+const body = document.querySelector("body");
+
+btn.forEach((ele) => {
+  ele.addEventListener("click", (e) => {
+    if (e.target.id === "gray") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "red") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "green") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
